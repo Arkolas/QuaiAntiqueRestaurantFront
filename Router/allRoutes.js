eda118979178f713b6974e3d444174e023926959
Arkolas <pilecerf@gmail.com>
2024-01-16 15:35:42 +0100

    test responsive

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,8 +3,12 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Galerie", "Glarie", "/pages/galerie.html", "/js/galerie.js"),
+    new Route("/Galerie", "Galerie", "/pages/galerie.html", "/pages/galerie.html", "/js/galerie.js"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
+
+
+
+
