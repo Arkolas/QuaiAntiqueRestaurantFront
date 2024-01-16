54e2ed35922e995021724253c87fd9a81dba29dc
Arkolas <pilecerf@gmail.com>
2024-01-16 16:40:25 +0100

    quaiantiquedebuggroutage

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,13 +2,7 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Galerie", "Galerie", "/pages/galerie.html", "/pages/galerie.html", "/js/galerie.js"),
-];
+    new Route("/", "Accueil", "/pages/home.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
-
-
-
-
